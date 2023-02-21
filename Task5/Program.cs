ab5baa4be6194801e7b8e857200797a54c0112bd
Loumann <bleyka80@gmail.com>
2023-02-22 00:36:50 +0500

    Добавьте файлы проекта.

---
+++ Task5/Program.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Task5_sort;
+
+namespace Task5
+{
+    class Program
+    {
+        public static string GlobalRev = "";
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string xuai = "";
+            string line = Console.ReadLine();
+            string vowels = "aeiouy";
+            string longestSubstring = "";
+            string wrongChars = "";
+
+
+            char[] charsEnglish = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] chars = line.ToCharArray();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            List<char> invalidCharsList = new List<char>();
+            Sort FuncStort = new Sort();
+
+
+            foreach (char c in line)
+            {
+                if (!charsEnglish.Contains(c))
+                {
+                    if (!invalidCharsList.Contains(c))
+                    {
+                        invalidCharsList.Add(c);
+                    }
+                }
+            }
+
+            if (invalidCharsList.Count > 0)
+            {
+                string invalidCharsString = string.Join(", ", invalidCharsList.ToArray());
+
+                Console.WriteLine($"Не подходящие символы: {invalidCharsString}");
+                Console.ReadKey();
+            }
+
+            foreach (char c in chars)
+            {
+                if (!charsEnglish.Contains(c))
+                {
+                    wrongChars += c + " ";
+                }
+            }
+
+
+            foreach (char c in chars)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount.Add(c, 1);
+                }
+            }
+
+
+
+            if (wrongChars != "")
+            {
+
+            }
+
+            else
+            {
+
+                if ((line.Length % 2) == 0)
+                {
+                    Console.Write("Четная обработаная строка: ");
+
+                    var str1 = line.Substring(0, line.Length / 2);
+                    var str2 = line.Substring(line.Length / 2, line.Length / 2);
+
+                    char[] charArray1 = str1.ToCharArray();
+                    char[] charArray2 = str2.ToCharArray();
+                    Array.Reverse(charArray1);
+                    Array.Reverse(charArray2);
+
+                    string s1 = new string(charArray1);
+                    string s2 = new string(charArray2);
+                    string LinePlus = s1 + s2;
+                    Console.WriteLine(LinePlus);
+                    xuai = LinePlus;
+                    GlobalRev = LinePlus;
+
+                }
+                else
+                {
+                    Console.Write("Не четная обработаная строка: ");
+
+                    char[] charArray1 = line.ToCharArray();
+                    Array.Reverse(charArray1);
+                    string a1 = new string(charArray1);
+                    string LineMinus = a1 + line;
+                    Console.WriteLine(LineMinus);
+                    xuai = LineMinus;
+                    GlobalRev = LineMinus;
+                    Console.WriteLine(GlobalRev);
+
+                }
+
+
+
+                for (int i = 0; i < xuai.Length; i++)
+                {
+                    for (int j = i + 1; j <= xuai.Length; j++)
+                    {
+                        string subString = xuai.Substring(i, j - i);
+
+                        if (vowels.Contains(subString[0]) && vowels.Contains(subString[subString.Length - 1]))
+                        {
+                            if (subString.Length > longestSubstring.Length)
+                            {
+                                longestSubstring = subString;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("Самая длинная подстрока, начинающаяся и заканчивающаяся гласной буквой, - это: " + longestSubstring);
+
+
+
+
+                Console.WriteLine("Cколько раз входил в обработанную строку каждый символ:");
+
+                foreach (KeyValuePair<char, int> kvp in charCount)
+                {
+                    Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+                }
+
+                FuncStort.Sorts();
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Task5/Sort.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5;
+
+namespace Task5_sort
+{
+    public class Sort
+    {
+        public void Sorts()
+        {
+            string inputString = Task5.Program.GlobalRev;
+            Console.Write("Выберите метод сортировки цифрой (Quicksort(1) / Tree Sort(2)): ");
+            string sortingAlgorithm = Console.ReadLine();
+
+            if (sortingAlgorithm == "1")
+            {
+                Quicksort(inputString);
+            }
+            else if (sortingAlgorithm == "2")
+            {
+                TreeSort(inputString);
+            }
+            else
+            {
+                Console.WriteLine("Не верно введенные данные");
+            }
+        }
+
+        public static void Quicksort(string inputString)
+        {
+            char[] inputArray = inputString.ToCharArray();
+            int left = 0;
+            int right = inputArray.Length - 1;
+
+            QuickSort(inputArray, left, right);
+
+            Console.WriteLine(inputArray);
+        }
+
+        public static void QuickSort(char[] inputArray, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(inputArray, left, right);
+
+                if (pivot > 1)
+                {
+                    QuickSort(inputArray, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSort(inputArray, pivot + 1, right);
+                }
+            }
+        }
+
+        public static int Partition(char[] inputArray, int left, int right)
+        {
+            char pivot = inputArray[left];
+            while (true)
+            {
+                while (inputArray[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (inputArray[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (inputArray[left] == inputArray[right])
+                        return right;
+
+                    char temp = inputArray[left];
+                    inputArray[left] = inputArray[right];
+                    inputArray[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public static void TreeSort(string inputString)
+        {
+            char[] inputArray = inputString.ToCharArray();
+            Tree tree = new Tree();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                tree.Insert(inputArray[i]);
+            }
+
+            tree.PrintInorder();
+        }
+
+
+
+        public class Tree
+        {
+            public char Value { get; set; }
+            public Tree Left { get; set; }
+            public Tree Right { get; set; }
+
+            public void Insert(char value)
+            {
+                if (this.Value == 0)
+                {
+                    this.Value = value;
+                }
+                else
+                {
+                    if (value < this.Value)
+                    {
+                        if (this.Left == null)
+                        {
+                            this.Left = new Tree();
+                        }
+                        this.Left.Insert(value);
+                    }
+                    else
+                    {
+                        if (this.Right == null)
+                        {
+                            this.Right = new Tree();
+                        }
+                        this.Right.Insert(value);
+                    }
+                }
+            }
+
+            public void PrintInorder()
+            {
+                if (this.Left != null)
+                {
+                    this.Left.PrintInorder();
+                }
+
+                Console.Write(this.Value + " ");
+
+                if (this.Right != null)
+                {
+                    this.Right.PrintInorder();
+                }
+            }
+        }
+    }
+}
